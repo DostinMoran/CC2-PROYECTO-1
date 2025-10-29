696988d3e9d5a944c494a217a3b060bd0ab2c679
DilanZamora <24004655@galileo.edu>
2025-10-29 12:03:16 -0600

    Se crea y termina el logging, todo funciona bien, se para a terminar la configuracion de run en Elevator.java

--- Elevator/Elevator.java
+++ Elevator/Elevator.java
@@ -1,15 +1,19 @@
 package Elevator;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.logging.Logger;
+import Elevator.components.ElevatorLogger;
+
 public class Elevator implements Movimientos, Runnable {
     static int cantidadElevadores = 1;
     protected boolean[] tableroInterno;
+    private final Logger mLogger;
     protected int id, tiempoEspera, duracionMovimiento, pisoActual, pisoDestino;
     protected boolean detenerElevadores;
     protected ConcurrentLinkedQueue<Integer> cola;
     protected Direccion direccion;
 
-    public Elevator(int tiempoEspera, int cantidadPisos, int duracionMovimiento){
+    public Elevator(int tiempoEspera, int cantidadPisos, int duracionMovimiento) {
         this.tiempoEspera = tiempoEspera;
         this.direccion = Direccion.UP;
         this.duracionMovimiento = duracionMovimiento;
@@ -22,37 +26,39 @@ public class Elevator implements Movimientos, Runnable {
             this.tableroInterno[i] = false;
         }
         this.detenerElevadores = false;
+        this.mLogger = ElevatorLogger.cache(this.id);
     }
 
-    public void mover(int duracionMovimiento){
+    public void mover(int duracionMovimiento) {
         for (int i = 0; i < this.tableroInterno.length; i++) {
             if (this.tableroInterno[i] == true) {
-                this.pisoDestino = i+1;
+                this.pisoDestino = i + 1;
                 if (this.pisoActual < this.pisoDestino) {
                     this.direccion = Direccion.UP;
-                    for (int j = this.pisoActual; j < pisoDestino ; j++) {
+                    for (int j = this.pisoActual; j < pisoDestino; j++) {
                         try {
                             Thread.sleep(duracionMovimiento);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                         this.pisoActual++;
-                    }    
+                    }
                 } else if (this.pisoActual > this.pisoDestino) {
                     this.direccion = Direccion.DOWN;
-                    for (int j = this.pisoActual; j > pisoDestino ; j--) {
+                    for (int j = this.pisoActual; j > pisoDestino; j--) {
                         try {
                             Thread.sleep(duracionMovimiento);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                         this.pisoActual--;
-                    }    
+                    }
                 }
             }
         }
     }
-    public void parada(int tiempoEspera){
+
+    public void parada(int tiempoEspera) {
         for (int i = 0; i < this.tableroInterno.length; i++) {
             if (this.tableroInterno[i] == true) {
                 this.tableroInterno[i] = false;
@@ -60,42 +66,42 @@ public class Elevator implements Movimientos, Runnable {
         }
     }
 
-    public int getId(){
+    public int getId() {
         return this.id;
     }
 
-    public int getPisoActual(){
+    public int getPisoActual() {
         return this.pisoActual;
     }
 
-    public int getPisoDestino(){
+    public int getPisoDestino() {
         return this.pisoDestino;
     }
 
-    public Direccion getDireccion(){
+    public Direccion getDireccion() {
         return this.direccion;
     }
 
-    public void resetElevador(){
+    public void resetElevador() {
         if (pisoActual >= 1) {
             this.direccion = Direccion.DOWN;
             this.pisoDestino = 1;
-            for (int i = this.pisoActual; i >= pisoDestino ; i--) {
+            for (int i = this.pisoActual; i >= pisoDestino; i--) {
                 this.pisoActual--;
-            }    
+            }
         }
     }
 
-    public void agregarSolicitud(int x){
+    public void agregarSolicitud(int x) {
         for (int i = 0; i < this.tableroInterno.length; i++) {
-            if (x == i+1) {
+            if (x == i + 1) {
                 this.tableroInterno[i] = true;
                 this.cola.add(x);
             }
         }
     }
 
-    public ConcurrentLinkedQueue<Integer> getsolicitudes(){
+    public ConcurrentLinkedQueue<Integer> getsolicitudes() {
         return this.cola;
     }
 
@@ -110,7 +116,7 @@ public class Elevator implements Movimientos, Runnable {
     @Override
     public void run() {
         while (!this.detenerElevadores) {
-            
+
         }
     }
 }
---
+++ Elevator/components/ElevatorLogger.java
@@ -0,0 +1,29 @@
+package Elevator.components;
+
+import java.util.logging.*;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+public final class ElevatorLogger {
+    
+    private ElevatorLogger() {
+    }
+
+    public static Logger cache(int id) {
+        Logger logger = Logger.getLogger("Elevador-" + id);
+        logger.setUseParentHandlers(true);
+
+        if (logger.getHandlers().length == 0) {
+            try {
+                FileHandler archivo = new FileHandler("Elevador-" + id + ".log", true);
+                archivo.setFormatter(new SimpleFormatter());
+                logger.addHandler(archivo);
+            } catch (IOException | NullPointerException e) {
+                throw new Error("ERROR: " + e.getMessage());
+            }
+        }
+        return logger;
+    }
+
+    
+}
